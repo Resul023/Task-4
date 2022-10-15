725e60a0eee39e5b01554e661cfcd8fb1041f844
Resul023 <73m6u7h@code.edu.az>
2022-10-15 20:15:48 +0400

    Task4 Finis

---
+++ Task4/assets/js/main.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector(".mobile-icon");
+let menuList = document.querySelector(".mobile-menu")
+menuBtn.addEventListener("click",function(){
+    menuList.classList.toggle("d-block");
+})
+
